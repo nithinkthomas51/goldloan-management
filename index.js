e7031322458068d48727eb13c502cbe9ae0e4fc9
Nithin K Thomas <64864340+nithinkthomas51@users.noreply.github.com>
2025-06-10 23:37:33 +0000

    Created a new route for customers which returns dummy in memory customer data

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import customerRoutes from './routes/customers.js'
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
+app.use('/customers', customerRoutes);
 
 app.get('/', (req, res) => {
     res.send('Test App');
---
+++ routes/customers.js
@@ -0,0 +1,35 @@
+import express from 'express'
+
+const router = express.Router();
+const customers = [
+    {
+        customerId: 1,
+        customerName: 'Nithin K Thomas',
+        contact: '+353904641188',
+        email: 'nithinkthomas51@gmail.com'
+    },
+    {
+        customerId: 2,
+        customerName: 'Naveen K Thomas',
+        contact: '++919207247410',
+        email: 'naveenkthomas51@gmail.com'
+    },
+    {
+        customerId: 1,
+        customerName: 'ABC',
+        contact: '+353904642288',
+        email: 'abc@gmail.com'
+    }
+];
+
+router.get('/', (req, res) => {
+    res.send(customers);
+});
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+    const customer = customers.find((customer) => customer.customerId === id);
+    res.send(customer);
+})
+
+export default router
